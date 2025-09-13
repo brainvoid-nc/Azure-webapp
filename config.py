3d4ea28a7afe5a5367d7474d4f6371666649842c
Nandini <nandinichatterjee2003@gmail.com>
2025-09-13 18:32:17 +0530

    commit changes

--- config.py
+++ config.py
@@ -15,7 +15,19 @@ class Config(object):
     SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'ENTER_SQL_SERVER_PASSWORD'
     # Below URI may need some adjustments for driver version, based on your OS, if running locally
     # SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://cmsadmin@azure-server-cms:CMS4dmin@azure-server-cms:1433/cms?driver=ODBC+Driver+18+for+SQL+Server'
-    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI") 
+    SQLALCHEMY_DATABASE_URI = (
+        "mssql+pyodbc://"
+        + SQL_USER_NAME
+        + "@"
+        + SQL_SERVER
+        + ":"
+        + SQL_PASSWORD
+        + "@"
+        + SQL_SERVER
+        + ":1433/"
+        + SQL_DATABASE
+        + "?driver=ODBC+Driver+17+for+SQL+Server&encrypt=yes"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     ### Info for MS Authentication ###
